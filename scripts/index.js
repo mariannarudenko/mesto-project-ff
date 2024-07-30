03c6ee717d241ed3e4f4ce0a9edf862813a1fde5
Marianna Rudenko <gavadgrami@gmail.com>
2024-07-30 15:49:32 +0500

    fix alt and delete card function

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,37 @@
-// создаю функцию, которая принимает два аргумента: объект карточки и коллбек для удаления 
+/**
+ * Создает элемент карточки на основе предоставленных данных.
+ * @param {Object} cardData - Данные для создания карточки.
+ * @param {string} cardData.link - Ссылка на изображение карточки.
+ * @param {string} cardData.name - Название карточки.
+ * @param {Function} deleteCallback - Функция обратного вызова для удаления карточки.
+ * @returns {HTMLElement} - Созданный элемент карточки.
+ */
 function createCard (cardData, deleteCallback) {
-  // находит и возвращает содержимое card-template
   const cardTemplate = document.querySelector('#card-template').content;
-  // клонирует элемент с классом places__item вместе с потомками
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
   
-  // находит элемент внутри клонированного шаблона и устанавливает значения из объекта cardData 
   cardElement.querySelector('.card__image').src = cardData.link;
+  cardElement.querySelector('.card__image').alt = cardData.name;
   cardElement.querySelector('.card__title').textContent = cardData.name;
 
-  // обработчик клика по кнопке с классом card__delete-button
   cardElement.querySelector('.card__delete-button').addEventListener('click', () => {
-    //вызывает функцию удаления на кликнутую карточку
     deleteCallback(cardData);
   });
-  // возвращает клонированный и заполненный элемент карточки
   return cardElement;
 }
 
-// находит и возвращает places__list
+/**
+ * Добавляет карточки в контейнер на основе массива данных.
+ * @param {Array<Object>} initialCards - Массив данных для карточек.
+ * @param {string} initialCards[].link - Ссылка на изображение карточки.
+ * @param {string} initialCards[].name - Название карточки.
+ */
 const cardContainer = document.querySelector('.places__list');
 
-//перебирает массив объектов и передает данные как cardData
 initialCards.forEach((cardData) => {
-  //передает данные для текущей карточки и коллбек-функцию для удаления
-  const cardElement = createCard(cardData, (data) => {
-    //карточка удаляется если пользователь кликает по кнопке
+  const cardElement = createCard(cardData, function deleteCard(data) {
     cardElement.remove();
   });
-  //добавляет созданный элемент в конец списка places__list
   cardContainer.appendChild(cardElement);
 });
 
